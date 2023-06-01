d0963c8e6c4e83f9569034097f8af760e9051f32
Yuri Souza <yurirs.santos@gmail.com>
2023-05-31 22:39:05 -0300

    AULA DE RENDER

--- recipes/views.py
+++ recipes/views.py
@@ -6,7 +6,7 @@ def home(request):
     return render(request, 'recipes/home.html')
 
 def contato(request):
-    return HttpResponse('Contato')
+    return render(request, 'recipes/contato.html')
 
 def sobre(request):
     return HttpResponse('Sobre')
