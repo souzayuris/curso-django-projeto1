67835110c0d9ee49f104c0972905d60e13f8aaf8
Yuri Souza <yurirs.santos@gmail.com>
2023-06-19 22:47:05 -0300

    dados_fakes

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,14 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+
+from utils.recipes.factory import make_recipe
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes' :[make_recipe() for _ in range(10)],
+    })
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+    })
