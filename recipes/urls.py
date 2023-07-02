d301a927d76551d6c6d1cbe4eb60775044671154
Yuri Souza <yurirs.santos@gmail.com>
2023-07-01 22:11:23 -0300

    MEDIA

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from . import views
@@ -9,3 +11,6 @@ urlpatterns = [
     path('recipes/<int:id>/', views.recipe, name="recipe"),
 
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
