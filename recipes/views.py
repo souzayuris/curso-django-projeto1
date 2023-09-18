8865dc15b785624ac54eeaa226b5d1c50acaffef
Yuri Souza <yurirs.santos@gmail.com>
2023-09-17 23:06:09 -0300

    Add Search

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 
@@ -47,7 +48,19 @@ def search(request):
 
     if not search_term:
         raise Http404()
+    
+    recipes = Recipe.objects.filter(
+        # where title like search_term
+        Q(
+            Q(title__icontains=search_term) |
+            Q(description__icontains=search_term),
+        ),
+        is_published=True
+    ).order_by('-id')
+    
 
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}"',
+        'search_term': search_term,
+        'recipes':recipes,
     })
