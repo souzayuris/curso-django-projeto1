931bcf86d02a3f6aa72d4eaff8a78ca7c28a113a
Yuri Souza <yurirs.santos@gmail.com>
2023-09-19 22:18:18 -0300

    Mais testes

--- recipes/tests/test_recipe_category_view.py
+++ recipes/tests/test_recipe_category_view.py
@@ -55,23 +55,4 @@ class RecipeCategoryViewTest(RecipeTestBase):
             response.content.decode('utf-8')
         )  
 
-    def test_recipe_search_uses_correct_view_function(self):
-        resolved = resolve(reverse('recipes:search'))
-        self.assertIs(resolved.func, views.search)
-
-    def test_recipe_search_loads_correct_template(self):
-        response = self.client.get(reverse('recipes:search') + '?q=teste')
-        self.assertTemplateUsed(response, 'recipes/pages/search.html')
-
-    def test_recipe_search_raises_404_if_no_search_term(self):
-        url = reverse('recipes:search')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 404)
-
-    def test_recipe_search_term_is_on_page_title_and_escape(self):
-        url = reverse('recipes:search') + '?q=Teste'
-        response = self.client.get(url)
-        self.assertIn(
-            'Search for &quot;Teste&quot;',
-            response.content.decode('utf-8')
-        )
+    
---
+++ utils/pagination.py
@@ -0,0 +1,6 @@
+def make_pagination_range(
+            page_range=list(range(1,21)),
+            qty_paginas=4,
+            current_page=1,
+        ):
+    return [1,2,3,4]
